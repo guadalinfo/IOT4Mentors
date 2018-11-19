c6d95c161bb059822c598f00a59250aacfc69225
Javacasm <javacasm@gmail.com>
2018-11-19 10:20:22 +0100

    update

--- codigo/LCD_DHT_Humedad_Rele/LCD_DHT_Humedad_Rele.ino
+++ codigo/LCD_DHT_Humedad_Rele/LCD_DHT_Humedad_Rele.ino
@@ -5,7 +5,7 @@
 
 #define Rele_2 D4
 
-LiquidCrystal_I2C lcd(0x27,16,2); // Cambiamos la direccion
+LiquidCrystal_I2C lcd(0x27,16,2); // Cambiamos la direccion a 0x27 0x30 o 0x3F
 DHT dht16(D6,DHT22);
 
 void setup()
@@ -26,13 +26,20 @@ void loop() {
     lcd.print("H:");
     lcd.print((dht16.readHumidity( )));
 
+
+
+    int humedad = 1024 - analogRead(A0);
+
     // Medida de humedad de suelo
     lcd.setCursor(10,0);
-    lcd.print(analogRead(A0));
+    lcd.print(humedad);
     lcd.print("   ");
 
+    int Humedad_GEranio = 512;
+    int Humedad_Lechuga = 322;
+    
     // Encendido y apagado del rele
-    if(analogRead(A0) > 500 ) {
+    if( humedad  > Humedad_GEranio ) {
       digitalWrite(Rele_2,LOW);
     } else {
       digitalWrite(Rele_2,HIGH);
